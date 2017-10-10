2247a87c1d032215d49029e1ea85d2daf6557985
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-10-10 11:40:22 -0400

    Add serial control, targetSpeed, breakTime

--- lab3/sensor_testing/sensor_testing.ino
+++ lab3/sensor_testing/sensor_testing.ino
@@ -8,10 +8,13 @@ int leftValue;
 int rightValue;
 boolean turningLeft = false;
 boolean turningRight = false;
+int targetSpeed;
+int breakTime;
+
+    
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(1); // M1
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2); // M2
-
 
 boolean isBlack(int analog_value){
   return (analog_value > 850 && analog_value < 930) ? true : false;
@@ -19,38 +22,57 @@ boolean isBlack(int analog_value){
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+  Serial.println("Starting Lince Tracer.");
+  Serial.println("To update break time, type breakTime,10. Default value is 10.");
+  Serial.println("To update targetSpeed, type targetSpeed,100. Default valule is 150.");
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  leftMotor->setSpeed(150);
+  targetSpeed = 150;
+  breakTime = 50;
+  leftMotor->setSpeed(targetSpeed);
   leftMotor->run(FORWARD);
   leftMotor->run(RELEASE);
-  rightMotor->setSpeed(150);
+  rightMotor->setSpeed(targetSpeed);
   rightMotor->run(FORWARD);
   rightMotor->run(RELEASE);
 }
 
 void loop() {
+    if (Serial.available()){
+       String cmd = Serial.readString();
+       byte delimIndex = cmd.indexOf(',');
+       String func = cmd.substring(0, delimIndex);
+       String value = cmd.substring(delimIndex+1);
+       if(func == "breakTime"){
+          breakTime = value.toInt();
+          Serial.println("Succesfully Updated breakTime to: " + value);
+       }
+       if(func == "targetSpeed"){
+          targetSpeed = value.toInt();
+          Serial.println("Succesfully targetSpeed to: " + value);
+       }
+    }
     boolean left = isBlack(analogRead(leftSensor));
     boolean right = isBlack(analogRead(rightSensor));
     if (!left && !right){
        turningLeft = false;
        turningRight = false;
-       leftMotor->setSpeed(150);
+       leftMotor->setSpeed(targetSpeed);
        leftMotor->run(FORWARD);
-       rightMotor->setSpeed(150);
+       rightMotor->setSpeed(targetSpeed);
        rightMotor->run(FORWARD);
     }
     else if(left && !right){
        if(!turningLeft){
            leftMotor->setSpeed(255);
-           leftMotor->run(BACKWARD); // breakkkkkkk
+           leftMotor->run(BACKWARD); // break
            rightMotor->setSpeed(255);
            rightMotor->run(BACKWARD);
            turningLeft = true;
+           delay(breakTime);
        }else{
-          rightMotor -> setSpeed(200);
+          rightMotor -> setSpeed(targetSpeed);
           rightMotor -> run(FORWARD);
        }
        
@@ -58,12 +80,13 @@ void loop() {
     else if(!left && right){
       if(!turningLeft){
            leftMotor->setSpeed(255);
-           leftMotor->run(BACKWARD); // breakkkkkkk
+           leftMotor->run(BACKWARD); // break
            rightMotor->setSpeed(255);
            rightMotor->run(BACKWARD);
            turningRight = true;
+           delay(breakTime);
        }else{
-          leftMotor -> setSpeed(200);
+          leftMotor -> setSpeed(targetSpeed);
           leftMotor -> run(FORWARD);
        }
     }
